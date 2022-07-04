e6bfc02e433b3b458d923a2d7725b9440918ff82
MH-RONY <mh.rony.dhanvi@gmail.com>
2022-07-05 03:17:23 +0600

    Last update - 05/07/2022

--- assets/js/swiper-custom.js
+++ assets/js/swiper-custom.js
@@ -1,10 +1,10 @@
 "use strict";
-$(document).ready(function() {
-var swiper = new Swiper('.swiper-container', {
-        pagination: '.swiper-pagination',
-        slidesPerView: 5,
-        paginationClickable: true,
-        spaceBetween: 30,
-        loop: true
-    });
+$(document).ready(function () {
+  var swiper = new Swiper(".swiper-container", {
+    pagination: ".swiper-pagination",
+    slidesPerView: 5,
+    paginationClickable: true,
+    spaceBetween: 30,
+    loop: true,
+  });
 });
